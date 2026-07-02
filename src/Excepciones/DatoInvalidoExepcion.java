0a6d34288915247211661889154efa81a2ccd43a
Fonse <Fonse@Joseline_Fon>
2026-07-01 21:47:18 -0600

    Se creo la capa modelo y su clase producto, y la capa de excepciones con dato invalido, producto duplicado y archivo

---
+++ src/Excepciones/DatoInvalidoExepcion.java
@@ -0,0 +1,15 @@
+
+package Excepciones;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class DatoInvalidoExepcion extends Exception{
+    public DatoInvalidoExepcion (String mensaje){
+    super(mensaje);
+    }
+
+}
---
+++ src/Excepciones/ProductoDuplicadoException.java
@@ -0,0 +1,14 @@
+
+package Excepciones;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class ProductoDuplicadoException extends Exception {
+    public ProductoDuplicadoException (String mensaje){
+    super (mensaje);
+    }
+}
